3cdde27d0561957e451e6c87a9ac3d9a61140cc6
EdwardMudaida <eddieigo8@gmail.com>
2017-07-10 18:04:27 +0300

    Edward Mudaida

--- app.rb
+++ app.rb
@@ -1,16 +1,43 @@
-require("sinatra")
-require("sinatra/reloader")
-also_reload("lib/**/*.rb")
+require('sinatra')
+require('sinatra/reloader')
 require('./lib/todo')
+require('./lib/list')
+also_reload('lib/**/*.rb')
+require("pg")
+
+DB = PG.connect({:dbname => "to_do"})
+
 
 get("/") do
-  @tasks = Task.all()
   erb(:index)
 end
 
-post("/task") do
-  description = params.fetch("description")
-  task = Task.new(description)
-  task.save()
+get("/lists/new") do
+  erb(:list_form)
+end
+
+post("/lists") do
+  name = params.fetch("name")
+  list = List.new({:name => name, :id => nil})
+  list.save()
   erb(:success)
+end
+
+get('/lists') do
+  @lists = List.all()
+  erb(:lists)
+end
+
+get("/lists/:id") do
+  @list = List.find(params.fetch("id").to_i())
+  erb(:list)
+end
+
+post("/tasks") do
+  description = params.fetch("description")
+  list_id = params.fetch("list_id").to_i()
+  @list = List.find(list_id)
+  @task = Task.new({:description => description, :list_id => list_id})
+  @task.save()
+  erb(:task_success)
 end
