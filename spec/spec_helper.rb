edb035ac1e71c6769d22577be76f5f5bc770a61d
EdwardMudaida <eddieigo8@gmail.com>
2017-07-18 16:58:33 +0300

    Remove instances of DB constant

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,13 +2,12 @@ require("sinatra/activerecord")
 require("rspec")
 require("pg")
 require("list")
-require("todo")
-
-ENV['RACK_ENV'] = 'test'
+require("task")
 
 RSpec.configure do |config|
   config.after(:each) do
-    DB.exec("DELETE FROM lists *;")
-    DB.exec("DELETE FROM tasks *;")
+    Task.all.each do |task|
+      task.destroy
+    end
   end
 end
