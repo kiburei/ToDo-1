6e4f5dbf22b80f41e907390323d3437799bf0cb9
EdwardMudaida <eddieigo8@gmail.com>
2017-07-18 16:57:54 +0300

    Remove tests for methods taken care of by Active record

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -1,6 +1,4 @@
-require('rspec')
-require('pg')
-require('list')
+
 require("spec_helper")
 
 DB = PG.connect({:dbname => 'to_do_test'})
@@ -33,72 +31,44 @@ describe(List) do
     end
   end
 
-  describe("#save") do
-    it("lets you save lists to the database") do
-      list = List.new({:name => "Epicodus stuff", :id => nil})
-      list.save() 
-      expect(List.all()).to(eq([list]))
-    end
+  it("deletes a list's tasks from the database") do
+    list = List.new({:name => "Moringa School stuff", :id => nil})
+    list.save()
+    task = Task.new({:description => "learn SQL", :list_id => list.id()})
+    task.save()
+    task2 = Task.new({:description => "Review Ruby", :list_id => list.id()})
+    task2.save()
+    list.delete()
+    expect(Task.all()).to(eq([]))
   end
+end
 
-    describe("#update")do 
-        it"lets you update lists in the database" do
-            list = List.new({:name => "Moringa school stuff", :id => nil})
-            list.save()
-            list.update({:name => "Homework stuff"})
-            expect(list.name()).to(eq("Homework stuff"))
-        end
-    end
-    
-    describe("#delete") do
-      it("lets you delete a list from the database") do
-        list = List.new({:name => "Moringa School stuff", :id => nil})
-        list.save()
-        list2 = List.new({:name => "House stuff", :id => nil})
-        list2.save()
-        list.delete()
-        expect(List.all()).to(eq([list2]))
-      end
-        
-        it("deletes a list's tasks from the database") do
-        list = List.new({:name => "Moringa School stuff", :id => nil})
-        list.save()
-        task = Task.new({:description => "learn SQL", :list_id => list.id()})
-        task.save()
-        task2 = Task.new({:description => "Review Ruby", :list_id => list.id()})
-        task2.save()
-        list.delete()
-        expect(Task.all()).to(eq([]))
-      end
-    end
-    
-  describe("#==") do
-    it("is the same list if it has the same name") do
-      list1 = List.new({:name => "Epicodus stuff", :id => nil})
-      list2 = List.new({:name => "Epicodus stuff", :id => nil})
-      expect(list1).to(eq(list2))
-    end
+describe("#==") do
+  it("is the same list if it has the same name") do
+    list1 = List.new({:name => "Epicodus stuff", :id => nil})
+    list2 = List.new({:name => "Epicodus stuff", :id => nil})
+    expect(list1).to(eq(list2))
   end
+end
 
-  describe(".find") do
-    it("returns a list by its ID") do
-      test_list = List.new({:name => "Moringaschool stuff", :id => nil})
-      test_list.save()
-      test_list2 = List.new({:name => "Home stuff", :id => nil})
-      test_list2.save()
-      expect(List.find(test_list2.id())).to(eq(test_list2))
-    end
+describe(".find") do
+  it("returns a list by its ID") do
+    test_list = List.new({:name => "Moringaschool stuff", :id => nil})
+    test_list.save()
+    test_list2 = List.new({:name => "Home stuff", :id => nil})
+    test_list2.save()
+    expect(List.find(test_list2.id())).to(eq(test_list2))
   end
+end
 
-  describe("#tasks") do
-    it("returns an array of tasks for that list") do
-      test_list = List.new({:name => "Moringaschool stuff", :id => nil})
-      test_list.save()
-      test_task = Task.new({:description => "Learn SQL", :list_id => test_list.id()})
-      test_task.save()
-      test_task2 = Task.new({:description => "Review Ruby", :list_id => test_list.id()})
-      test_task2.save()
-      expect(test_list.tasks()).to(eq([test_task, test_task2]))
-    end
+describe("#tasks") do
+  it("returns an array of tasks for that list") do
+    test_list = List.new({:name => "Moringaschool stuff", :id => nil})
+    test_list.save()
+    test_task = Task.new({:description => "Learn SQL", :list_id => test_list.id()})
+    test_task.save()
+    test_task2 = Task.new({:description => "Review Ruby", :list_id => test_list.id()})
+    test_task2.save()
+    expect(test_list.tasks()).to(eq([test_task, test_task2]))
   end
 end
