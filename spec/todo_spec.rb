822ed895cf793a9412d35d504db2c231fe36c170
EdwardMudaida <eddieigo8@gmail.com>
2017-07-18 16:58:57 +0300

    Remove tests for methods taken care of by Active record

--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -1,18 +1,9 @@
-require('todo')
 require("spec_helper")
 
 describe(Task) do
   describe(".all") do
     it("is empty at first") do
       expect(Task.all()).to(eq([]))
-    end
-  end
-
-  describe("#save") do
-    it("adds a task to the array of saved tasks") do
-      test_task = Task.new({:description => "learn SQL", :list_id => 1})
-      test_task.save()
-      expect(Task.all()).to(eq([test_task]))
     end
   end
 
