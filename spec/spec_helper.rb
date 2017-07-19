b9b517c8e6fd5d3f0217064417c188c1c8bf7623
EdwardMudaida <eddieigo8@gmail.com>
2017-07-19 08:22:00 +0300

    Use Shortcuts with Bundler and Dir

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,13 @@
-require("sinatra/activerecord")
-require("rspec")
-require("pg")
-require("list")
-require("task")
+ENV['RACK_ENV'] = 'test'
+
+require 'bundler/setup'
+Bundler.require(:default, :test)
+set(:root, Dir.pwd())
+require 'capybara/rspec'
+Capybara.app = Sinatra::Application
+set(:show_exceptions. false)
+require './app'
+Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each { |file| require file }
 
 RSpec.configure do |config|
   config.after(:each) do
